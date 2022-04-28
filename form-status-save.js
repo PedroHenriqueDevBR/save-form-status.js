ac1551601a1044f53382f5be74d6055db952d63c
Pedro Henrique <36716898+PedroHenriqueDevBR@users.noreply.github.com>
2022-04-28 10:46:50 -0300

    Updated script

--- form-status-save.js
+++ form-status-save.js
@@ -1,29 +1,38 @@
-var inputs = document.getElementsByTagName('input');
-var selects = document.getElementsByTagName('select');
+var inputs = undefined;
+var selects = undefined;
 
-function salvarStatus() {
-  var input_objects = [];
-
-  for (var input of inputs) input_objects.push({
-    'id': input.id,
-    'value': input.value,
-  });
-  for (var select of selects) input_objects.push({
-    'id': select.id,
-    'value': select.value,
-  });
-  
-  localStorage.setItem('produto_form_status', JSON.stringify(input_objects));
+function carregarInputs() {
+    inputs = document.getElementsByTagName('input');
+    selects = document.getElementsByTagName('select');
 }
 
-function verificarStatusSalvo() {
-  var data_form = localStorage.getItem('produto_form_status');
-  if (data_form != null) {
-      var input_objects = JSON.parse(data_form);
-      for (var input of input_objects) {
-          if (input.id.length > 0) {
-              document.getElementById(input.id).value = input.value;
-          }
-      }
-  }
+function salvarStatus(nome_form) {
+    carregarInputs();
+    if (inputs != undefined && selects != undefined) {
+        var input_objects = [];
+
+        for (var input of inputs) input_objects.push({
+            'id': input.id,
+            'value': input.value,
+        });
+        for (var select of selects) input_objects.push({
+            'id': select.id,
+            'value': select.value,
+        });
+
+        localStorage.setItem(nome_form, JSON.stringify(input_objects));
+    }
+}
+
+function verificarStatusSalvo(nome_form) {
+    carregarInputs();
+    var data_form = localStorage.getItem(nome_form);
+    if (data_form != null) {
+        var input_objects = JSON.parse(data_form);
+        for (var input of input_objects) {
+            if (input.id.length > 0) {
+                document.getElementById(input.id).value = input.value;
+            }
+        }
+    }
 }
